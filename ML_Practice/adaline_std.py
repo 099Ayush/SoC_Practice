9f7bbe3bea8f4c14caf7f0e8b4df2189552d0fcc
099Ayush <52877162+099Ayush@users.noreply.github.com>
2020-04-19 23:17:56 +0530

    Update adaline_std.py

--- ML_Practice/adaline_std.py
+++ ML_Practice/adaline_std.py
@@ -5,68 +5,60 @@ import pandas as pd
 
 class AdalineGD(object):
 
-    def __init__(self, f_eta=0.01, f_n_epochs=100, f_seed=1):
-        self.eta = f_eta
-        self.n_epochs = f_n_epochs
-        self.seed = f_seed
+    def __init__(self, eta=0.01, n_iter=100, seed=1):
+        self.eta = eta
+        self.n_epochs = n_iter
+        self.seed = seed
 
-    def fit(self, f_X, f_y):
-        self.w_ = np.random.RandomState(self.seed).normal(loc=0.0, scale=1.0, size=1 + f_X.shape[1])
+    def fit(self, X, y):
+        self.w_ = np.random.RandomState(self.seed).normal(loc=0.0, scale=1.0, size=1 + X.shape[1])
         self.cost_ = []
         for _ in range(self.n_epochs):
-            net_input = f_X.dot(self.w_[1:]) + self.w_[0]
+            net_input = X.dot(self.w_[1:]) + self.w_[0]
             output = self.activation(net_input)
-            errors = f_y - output
-            self.w_[1:] += self.eta * f_X.T.dot(errors)
+            errors = y - output
+            self.w_[1:] += self.eta * X.T.dot(errors)
             self.w_[0] += self.eta * errors.sum()
             self.cost_.append((errors ** 2).sum() / 2.0)
 
         return self
 
-    def activation(self, f_X):
-        return f_X
+    def activation(self, X):
+        return X
 
-    def predict(self, f_X):
-        net_input = f_X.dot(self.w_[1:]) + self.w_[0]
+    def predict(self, X):
+        net_input = X.dot(self.w_[1:]) + self.w_[0]
         return np.where(net_input >= 0, 1, -1)
 
 
-n_epochs = int(input('The number of repetitions (epochs): '))
-eta = float(input('Learning Rate, eta: '))
-seed = int(input('Seed value (enter a random integer): '))
-
-df = pd.read_csv('data.csv')
-X = df[['0', '2']][:100].values  # Training examples as points on 2-d plane.
-
-for i in range(2):
-    X[:, i] = (X[:, i] - X[:, i].mean()) / X[:, i].std()
-
-y = df['4'][:100].replace('Iris-setosa', 1).replace('Iris-versicolor', -1).values    # Target values.
-
-ppn = AdalineGD(f_n_epochs=n_epochs, f_eta=eta, f_seed=seed)
-ppn.fit(X, y)   # Train the model.
-
-# To create a filled contour plot consisting of division hyperplane(here a straight line):-
-x1 = np.arange(X[:, 0].min() - 1, X[:, 0].max() + 1, 0.01)
-x2 = np.arange(X[:, 1].min() - 1, X[:, 1].max() + 1, 0.01)
-
-xx1, xx2 = np.meshgrid(x1, x2)
-
-points = np.array(list(zip(xx1.ravel(), xx2.ravel())))
-z = []
-for point in points:
-    z.append(ppn.predict(point))
-z = np.array(z)
-z = z.reshape(xx1.shape)
-plt.figure(figsize=(12, 5))
+plt.figure(figsize=(14, 5))
 plt.subplot(121)
-plt.scatter(X[:50, 0], X[:50, 1], marker='o') # Scatter plot for Setosa Iris flowers.
-plt.scatter(X[50:, 0], X[50:, 1], marker='x') # Scatter plot for Versicolor Iris flowers.
-plt.contourf(x1, x2, z, alpha=0.3)
-plt.xlabel('Sepal Length: cm')
-plt.ylabel('Petal length: cm')
+df = pd.read_csv('data.csv')
+X = df[['0', '2']].values[:100]
+for i in range(2):  # To standardize the dataset.
+    X[:, i] = (X[:, i] - X[:, i].mean()) / X[:, i].std()
+plt.scatter(X[:50, 0], X[:50, 1])
+plt.scatter(X[50:, 0], X[50:, 1])
+y = np.array([1 for t in range(50)] + [-1 for t in range(50)])
+
+i_eta = float(input('Enter the learning rate: '))
+i_n_iter = int(input('Enter the number of passes over the training dataset: '))
+i_seed = int(input('Enter the seed for the random generator: '))
+
+ad_std = AdalineGD(eta=i_eta, n_iter=i_n_iter, seed=i_seed).fit(X, y)
+rn = np.arange(X[:, 0].min(), X[:, 0].max(), 0.01)
+
+
+def f(x):   # This function represents the classification hyperplane(line)
+    w = ad_std.w_
+    return (-x * w[1] - w[0]) / w[2]
+
+
+plt.plot(rn, f(rn), 'g')
+plt.xlabel('Sepal Length (standardized): cm')
+plt.ylabel('Petal length (standardized): cm')
 plt.subplot(122)
-plt.plot(range(1, len(ppn.cost_) + 1), np.log10(ppn.cost_), marker='o')
-plt.xlabel('Epoch number')
-plt.ylabel('Logarithm of Cost Function(sum of squared errors)')
+plt.xlabel('Epoch Number')
+plt.ylabel('Logarithm(10) of Cost Function')
+plt.plot(range(1, len(ad_std.cost_) + 1), np.log10(ad_std.cost_))
 plt.show()
