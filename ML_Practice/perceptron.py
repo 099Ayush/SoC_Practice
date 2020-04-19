5fccc87ef9e43f88aa06168f39eebd939e87e078
099Ayush <52877162+099Ayush@users.noreply.github.com>
2020-04-19 23:16:52 +0530

    Update perceptron.py

--- ML_Practice/perceptron.py
+++ ML_Practice/perceptron.py
@@ -4,20 +4,20 @@ import pandas as pd
 
 class Perceptron(object):
 
-    def __init__(self, f_n_epochs=1000, f_eta=0.001, f_seed=1):
-        self.n_epochs = f_n_epochs    # Number of passes over training dataset
-        self.eta = f_eta  # The training rate
-        self.seed = f_seed    # Seed to generate random weight vector, can be used to regain same result
+    def __init__(self, n_iter=1000, eta=0.001, seed=1):
+        self.n_iter = n_iter    # Number of passes over training dataset
+        self.eta = eta  # The training rate
+        self.seed = seed    # Seed to generate random weight vector, can be used to regain same result
 
-    def fit(self, f_X, f_y):
+    def fit(self, X, y):
 
         # Generate random weight vector, one extra element as the bias unit.
-        self.w_ = np.random.RandomState(self.seed).normal(loc=0.0, scale=1.0, size=1 + f_X.shape[1])
+        self.w_ = np.random.RandomState(self.seed).normal(loc=0.0, scale=1.0, size=1 + X.shape[1])
         self.errors_ = []   # A measure of how well the model performs after enough number of epochs.
 
-        for _ in range(self.n_epochs):
+        for _ in range(self.n_iter):
             n_errors = 0
-            for x, target in zip(f_X, f_y):     # Execute the perceptron learning algorithm
+            for x, target in zip(X, y):     # Execute the perceptron learning algorithm
                 predicted = self.predict(x)
                 error = target - predicted
                 self.w_[1:] += self.eta * error * x
@@ -27,45 +27,38 @@ class Perceptron(object):
 
         return self
 
-    def predict(self, f_X):  # Function to make prediction about a new dataset,
-                                # or one from the training dataset while learning.
-        value = f_X.dot(self.w_[1:]) + self.w_[0]
+    def predict(self, X):  # Function to make prediction about a new dataset,
+                           # or one from the training dataset while learning.
+        value = X.dot(self.w_[1:]) + self.w_[0]
         return np.where(value >= 0, 1, -1)
 
 
-n_epochs = int(input('The number of repetitions (epochs): '))
-eta = float(input('Learning Rate, eta: '))
-seed = int(input('Seed value (enter a random integer): '))
-
-df = pd.read_csv('data.csv')
-X = df[['0', '2']][:100].values  # Training examples as points on 2-d plane.
-
-y = df['4'][:100].replace('Iris-setosa', 1).replace('Iris-versicolor', -1).values    # Target values.
-
-ppn = Perceptron(f_n_epochs=n_epochs, f_eta=eta, f_seed=seed)
-ppn.fit(X, y)   # Train the model.
-
-# To create a filled contour plot consisting of division hyperplane(here a straight line):-
-x1 = np.arange(X[:, 0].min() - 1, X[:, 0].max() + 1, 0.01)
-x2 = np.arange(X[:, 1].min() - 1, X[:, 1].max() + 1, 0.01)
-
-xx1, xx2 = np.meshgrid(x1, x2)
-
-points = np.array(list(zip(xx1.ravel(), xx2.ravel())))
-z = []
-for point in points:
-    z.append(ppn.predict(point))
-z = np.array(z)
-z = z.reshape(xx1.shape)
-plt.figure(figsize=(12, 5))
+plt.figure(figsize=(14, 5))
 plt.subplot(121)
-plt.scatter(X[:50, 0], X[:50, 1], marker='o') # Scatter plot for Setosa Iris flowers.
-plt.scatter(X[50:, 0], X[50:, 1], marker='x') # Scatter plot for Versicolor Iris flowers.
-plt.contourf(x1, x2, z, alpha=0.3)
+df = pd.read_csv('data.csv')
+X = df[['0', '2']].values[:100]
+plt.scatter(X[:50, 0], X[:50, 1])
+plt.scatter(X[50:, 0], X[50:, 1])
+y = np.array([1 for t in range(50)] + [-1 for t in range(50)])
+
+i_eta = float(input('Enter the learning rate: '))
+i_n_iter = int(input('Enter the number of passes over the training dataset: '))
+i_seed = int(input('Enter the seed for the random generator: '))
+
+ppn = Perceptron(eta=i_eta, n_iter=i_n_iter, seed=i_seed).fit(X, y)
+rn = np.arange(X[:, 0].min(), X[:, 0].max(), 0.01)
+
+
+def f(x):   # This function represents the classification hyperplane(line)
+    w = ppn.w_
+    return (-x * w[1] - w[0]) / w[2]
+
+
+plt.plot(rn, f(rn), 'g')
 plt.xlabel('Sepal Length: cm')
 plt.ylabel('Petal length: cm')
 plt.subplot(122)
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel('Epoch number')
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_)
+plt.xlabel('Epoch Number')
 plt.ylabel('Number of Updates')
 plt.show()
