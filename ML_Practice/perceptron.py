60cddf05ba031ee5f1d76ee6a049e4dc19c5eaea
099Ayush <52877162+099Ayush@users.noreply.github.com>
2020-04-19 10:49:49 +0530

    Update perceptron.py

--- ML_Practice/perceptron.py
+++ ML_Practice/perceptron.py
@@ -32,24 +32,25 @@ class Perceptron(object):
         return np.where(value >= 0, 1, -1)
 
 
+n_epochs = int(input('The number of repetitions (epochs): '))
+eta = float(input('Learning Rate, eta: '))
+seed = int(input('Seed value (enter a random integer): '))
+
 df = pd.read_csv('data.csv')
 df1 = df[df['4'] == 'Iris-setosa']
 df2 = df[df['4'] == 'Iris-versicolor']
 ls11 = [t[0] for t in df1[['0']].values]
-ls12 = [t[0] for t in df1[['1']].values]
+ls12 = [t[0] for t in df1[['2']].values]
 ls21 = [t[0] for t in df2[['0']].values]
-ls22 = [t[0] for t in df2[['1']].values]
-
-plt.scatter(ls11, ls12, marker='o') # Scatter plot for Setosa Iris flowers.
-plt.scatter(ls21, ls22, marker='x') # Scatter plot for Versicolor Iris flowers.
+ls22 = [t[0] for t in df2[['2']].values]
 
 X = np.array(list(zip(ls11 + ls21, ls12 + ls22)))   # Training examples as points on 2-d plane.
 y = np.array([1 for t in range(len(ls11))] + [-1 for t in range(len(ls21))])    # Target values.
 
-ppn = Perceptron()
+ppn = Perceptron(n_epochs=n_epochs, eta=eta, seed=seed)
 ppn.fit(X, y)   # Train the model.
 
-# To create a filled contour plot consisting of division hyperplane(here a straight line)
+# To create a filled contour plot consisting of division hyperplane(here a straight line):-
 x1 = np.arange(X[:, 0].min() - 1, X[:, 0].max() + 1, 0.01)
 x2 = np.arange(X[:, 1].min() - 1, X[:, 1].max() + 1, 0.01)
 
@@ -61,5 +62,15 @@ for point in points:
     z.append(ppn.prediction(point))
 z = np.array(z)
 z = z.reshape(xx1.shape)
+plt.figure(figsize=(12, 5))
+plt.subplot(121)
+plt.scatter(ls11, ls12, marker='o') # Scatter plot for Setosa Iris flowers.
+plt.scatter(ls21, ls22, marker='x') # Scatter plot for Versicolor Iris flowers.
 plt.contourf(x1, x2, z, alpha=0.3)
+plt.xlabel('Sepal Length: cm')
+plt.ylabel('Petal length: cm')
+plt.subplot(122)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_)
+plt.xlabel('Epoch number')
+plt.ylabel('Number of updates')
 plt.show()
